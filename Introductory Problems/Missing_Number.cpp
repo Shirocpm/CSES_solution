44f54157f585f10a8a78dd5a4df82507d693cf2c
Asahi_Shiro <143000129+Shirocpm@users.noreply.github.com>
2024-04-08 13:00:56 +0800

    Create Missing_Number.cpp

---
+++ Introductory Problems/Missing_Number.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+#define IOS ios_base::sync_with_stdio(false);cin.tie();cout.tie();
+#define all(x) x.begin(), x.end()
+#define int long long
+#define pi pair<int, int >
+#define pq priority_queue
+#define lb lower_bound
+#define ub upper_bound
+#define pb push_back
+#define eb emplace_back
+#define mset multiset
+#define F first
+#define S second
+
+using namespace std;
+
+void solve ()
+{
+  cin >> n;
+  s = (n * (n + 1)) / 2;
+	for (int i = 1; i < n; i++) {cin >> a; s -= a;}
+	cout << s;
+}
+signed main(){IOS solve(); return 0;}
